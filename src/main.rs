18f832b2b40012aee6eb551de7b697c5834ab811
felden <michael.serra1@outlook.com>
2025-01-12 19:44:41 -0800

    toml setup now, first build

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+#![allow(non_snake_case)]
+use std::env;
+
 fn main() {
 
-    /// Inital setup of crates publish
+    // get input
+    
+    let args: Vec<String> = env::args().collect();
 
-    println!("Hello, world!");
+    match args.get(1) {
+        Some(command) => {
+            match command.as_str() {
+                "hello" => println!("hello ! how are you ?"),
+                "-h" | "--help" => print_Help(),
+                _ => println!("no commands : try -h for help ."),
+            }
+        }
+        None => println!("Welcome . try -h for help . "),
+    }
 }
+
+fn print_Help()
+{
+    println!("Hello ! How to use feld :");
+    println!("\n");
+    println!("  hello       Print a greeting !");
+    println!("  -h          Show this message");
+    println!("  goodbye     Print a farewell");
+    println!("  quote       Print a random quote");
+}
+
+
